504a5278d9083add0d00c7c5b321a28c6a9c5fc3
Vic-Nas <155357371+Vic-Nas@users.noreply.github.com>
2024-12-13 06:36:00 -0500

    Update 13_2024.py

--- 2024/13_2024.py
+++ 2024/13_2024.py
@@ -1,16 +1,13 @@
-inp = list(map(str.strip, open("input.txt", "r").readlines()))
-
-prize = lambda tup: 3 * tup[0] + tup[1]
 cut2 = lambda s: int(s[2:])
 
 def check(inp, error = 0):
-    i = 0
-    res = []
+    i = 0; res = []
     while i < len(inp):
         data = {"A": tuple(map(cut2, inp[i][10:].split(", "))), 
                 "B": tuple(map(cut2, inp[i + 1][10:].split(", "))), 
                 "P": tuple(map(cut2, inp[i + 2][7:].split(", ")))}
         den = data["A"][0] * data["B"][1] - data["B"][0] * data["A"][1]
+        
         if den:
             a = (data["P"][0] + error) * data["B"][1] - (data["P"][1] + error) * data["B"][0]
             b = (data["P"][1] + error) * data["A"][0] - (data["P"][0] + error) * data["A"][1]
@@ -18,7 +15,8 @@ def check(inp, error = 0):
         i += 4
     return res
 
-res1 = filter(lambda tup: tup[0] <= 100 and tup[1] <= 100, check(inp))
-res2 = check(inp, 10 ** 13)
-print("\nPart1:", sum(prize(tup) for tup in res1), "\nPart2:", sum(prize(tup) for tup in res2))
-print()
+inp = list(map(str.strip, open("input.txt", "r").readlines()))
+prize = lambda tup: 3 * tup[0] + tup[1]
+
+print("\nPart1:", sum(prize(tup) for tup in filter(lambda t: t[0] <= 100 and t[1] <= 100, check(inp))))
+print("Part2:", sum(prize(tup) for tup in check(inp, 10 ** 13)), "\n")
