5867d64c8361d1c00cbba6dcffb3d1260e4bdec0
Vic-Nas <nasci.victorio@gmail.com>
2024-12-14 09:59:19 -0500

    day 14 update

--- 2024/14_2024.py
+++ 2024/14_2024.py
@@ -1,13 +1,14 @@
+# Bruteforce pypy < 0.1s python < 4mins
+
 X, Y = 101, 103
 
 class Point:
-    def __init__(self, t1, t2 = (0, 1)):
+    def __init__(self, t1 = (0, 0), t2 = (1, 0)):
         self.x, self.y = t1
         self.dx, self.dy = t2
     
-    def next(self, dx = 0, dy = 0): 
-        if not (dx or dy): dx, dy = self.dx, self.dy
-        return Point(((self.x + dx) % X, (self.y + dy) % Y), (self.dx, self.dy))
+    def next(self): 
+        return Point(((self.x + self.dx) % X, (self.y + self.dy) % Y), (self.dx, self.dy))
     
     def quadrant(self):
         if 0 <= self.y < Y // 2:
@@ -24,7 +25,7 @@ def display(points):
     for p in points: map[p.y][p.x] = "*"
     print("\n".join("".join(row) for row in map))
 
-def look_like_sapin(points):
+def looks_like_sapin(points):
     for p in points:
         #   *
         x = p.x; y = p.y
@@ -32,7 +33,7 @@ def look_like_sapin(points):
         base_1 = Point((x + 1, y - 1)), Point((x + 1, y)), Point((x + 1, y + 1)) 
         # *****
         base_2 = Point((x + 2, y - 2)), Point((x + 2, y - 1)), Point((x + 2, y)), Point((x + 2, y + 1)), Point((x + 2, y + 2))
-        
+
         if all(p in points for p in base_1 + base_2): return True
     
     return False
@@ -46,15 +47,18 @@ the_map = points.copy()
 for i in range(100):
     the_map = list(map(Point.next, the_map))
     quadrants = {}
-    for point in the_map: 
+    for point in the_map:
         q = point.quadrant()
         if q: quadrants[q] = quadrants.get(q, 0) + 1
 print("\nPart1:", eval("*".join(map(str, quadrants.values()))))
 
-for i in range(10000):
-    points = list(map(Point.next, points))
-    if look_like_sapin(points):
-        print("Part2:", i + 1, "\n")
+the_map = points.copy()
+count = 0
+while True:
+    count += 1
+    the_map = list(map(Point.next, the_map))
+    if looks_like_sapin(the_map):
+        print("Part2:", count, "\n")
         break
 
-# display(points)
+# display(the_map)
